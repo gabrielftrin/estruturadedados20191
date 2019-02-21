02c7e542c0cd985ada0de61f350f431e7ee407e3
gabrielftrin <mrgabrielfernandes@hotmail.com>
2019-02-21 18:16:51 -0300

    Update and rename lista.c to lista_linear.c

---
+++ lista_linear.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Funções
+
+//Criar lista vazia
+//Inserir no final
+//Retirar o último
+//Localizar e alterar um elemento
+//Combinar 2 ou mais listas
+//Partir a lista
+//Copiar a lista
+//Ordenar ascendente ou descendente
+//Pesquisar a ocorrencia de item com valor particular
+
+int main()
+{
+   printf("Hello, World!");
+   return 0;
+}
